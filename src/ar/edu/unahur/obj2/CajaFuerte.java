a26ced29f03ee17e92e4085609b72799b0cb5800
Hernan <hjviltez@gmail.com>
2024-08-28 15:20:25 -0300

    creacion con test 28/8

---
+++ src/ar/edu/unahur/obj2/CajaFuerte.java
@@ -0,0 +1,56 @@
+package ar.edu.unahur.obj2;
+
+public class CajaFuerte {
+
+    private Integer reset;
+    private Boolean estaCerrada;
+    private Integer clave;
+    private Boolean puertaAbierta;
+    private Integer intentos;
+
+
+    public CajaFuerte(Integer reset) {
+        this.reset = reset;
+        this.puertaAbierta = true;
+        this.clave = null;
+        this.estaCerrada = false;
+        this.intentos = 2;
+    }
+
+    public Integer getClave() {
+        return clave;
+    }
+    public Boolean getEstaCerrada() {
+        return estaCerrada;
+    }
+    public void setPuertaAbierta(Boolean puertaAbierta) {
+        this.puertaAbierta = puertaAbierta;
+    }
+    public Boolean tieneClave(){
+            return clave == null;
+    }
+    public void crearClave(Integer clave) {
+        this.clave = clave;
+    }
+    public void cerrarCajaFuerte(Integer clave) {
+        if (puertaAbierta){
+            System.out.println("cierre la puerta antes"); //preguntar si se hacen test a los get
+        } else if (this.clave != clave) {
+            System.out.println("clave incorrecta");
+        }else {
+            this.estaCerrada = true;
+        }
+    }
+    public void abrirCajaFuerte(Integer clave) {
+        if (intentos < 0) {
+            System.out.println("bloqueada, llame al conserje");
+            intentos -= 1;
+        } else if (this.clave != clave) {
+            System.out.println("clave incorrecta, le quedan " + intentos + " intentos");
+            intentos -= 1;
+        } else {
+            this.estaCerrada = false;
+        }
+    }
+
+}
---
+++ test/ar/edu/unahur/obj2/CajaFuerteTest.java
@@ -0,0 +1,66 @@
+package ar.edu.unahur.obj2;
+
+import org.junit.Assert;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class CajaFuerteTest {
+    @Test
+    public void creacionCajaFuerte(){
+        //preparacion
+        Integer reset = 4321;
+        CajaFuerte cajaFuerte1 = new CajaFuerte(reset);
+        Boolean valorEsperado = true;
+        //ejecucion
+        Boolean valorObtenido = cajaFuerte1.tieneClave();
+        //contrastacion
+        assertEquals(valorEsperado,valorObtenido);
+        //limpieza
+    }
+
+    CajaFuerte cajaFuerte = new CajaFuerte(4321);
+    @Test
+    public void crearClave(){
+        // //preparacion
+        Integer clave = 2345;
+        Boolean valorEsperado = true;
+        //ejecucion
+        cajaFuerte.crearClave(clave);
+        Boolean valorObtenido =  cajaFuerte.getClave()==clave;
+        //contrastacion
+        assertEquals(valorEsperado,valorObtenido);
+    }
+
+    @Test
+    public void cerrarCajaFuerte(){
+        // //preparacion
+        Integer clave = 2345;
+        cajaFuerte.crearClave(clave); // o setClave = clave ????????
+        cajaFuerte.setPuertaAbierta(false);
+        Boolean valorEsperado = true;
+        //ejecucion
+        cajaFuerte.cerrarCajaFuerte(clave);
+        Boolean valorObtenido = cajaFuerte.getEstaCerrada();
+        //contrastacion
+        assertEquals(valorEsperado,valorObtenido);
+    }
+
+    @Test
+    public void abrirCajaFuerte(){
+        //preparacion
+        Integer clave = 235;
+        cajaFuerte.crearClave(clave);   //genero clave
+        cajaFuerte.setPuertaAbierta(false); //cierro puerta
+        cajaFuerte.cerrarCajaFuerte(clave); // cierro la caja
+        Boolean valorEsperado = false;
+        //ejecucion
+        cajaFuerte.abrirCajaFuerte(1);
+        cajaFuerte.abrirCajaFuerte(2);
+        cajaFuerte.abrirCajaFuerte(clave);
+        //cajaFuerte.abrirCajaFuerte(clave);
+        Boolean valorObtenido = cajaFuerte.getEstaCerrada();
+        //contrastacion
+        assertEquals(valorEsperado,valorObtenido);
+    }
+
+}
